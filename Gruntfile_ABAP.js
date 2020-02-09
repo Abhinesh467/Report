97ec1f98b3bb29f6a5a5c7c5cc97d0830ae4adeb
Abhinesh467 <60090214+Abhinesh467@users.noreply.github.com>
2020-02-09 13:11:11 +0530

    Update Gruntfile_ABAP.js

--- Gruntfile_ABAP.js
+++ Gruntfile_ABAP.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
     // Project specific variables
     var abapDevelopmentUser = process.env.ABAP_DEVELOPMENT_USER;
     var abapDevelopmentPassword = process.env.ABAP_DEVELOPMENT_PASSWORD;
-    var abapDevelopmentServer = "solman:3312";
+    var abapDevelopmentServer = "solman:8010";
     var abapDevelopmentInstance = process.env.ABAP_DEVELOPMENT_INSTANCE;
     var abapDevelopmentClient = process.env.ABAP_DEVELOPMENT_CLIENT;
     var abapApplicationName = process.env.ABAP_APPLICATION_NAME;
