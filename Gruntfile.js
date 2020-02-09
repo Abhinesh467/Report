f7eaddb1f6d0b181d748c7c39cbe9a30bb149c72
Abhinesh467 <60090214+Abhinesh467@users.noreply.github.com>
2020-02-09 23:45:34 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
             "use strict";
-            grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+            grunt.loadNpmTasks("sap/grunt-sapui5-bestpractice-build");
             grunt.config.merge({ compatVersion: "1.38" });
             grunt.registerTask("default", [
                         "clean",
