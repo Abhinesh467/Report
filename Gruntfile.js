f0c573c39224857c415f7e47b8531e92e2b5c992
Abhinesh467 <60090214+Abhinesh467@users.noreply.github.com>
2020-02-10 00:40:58 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,12 @@
+module.exports = function (grunt) {
+    'use strict';
+    grunt.loadNpmTasks('grunt-sapui5');
+};
+
+
+
+
+
 // module.exports = function (grunt) {
 //             "use strict";
 //             grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
@@ -8,23 +17,24 @@
 //                         "build"
 //             ]);
 // };
-module.exports = function (grunt) {
-	"use strict";
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	grunt.config.merge({ compatVersion: "edge" });
-	grunt.registerTask("default", [
-		"clean",
-		"lint",
-		"build"
-	]);
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
-	grunt.registerTask("unit_and_integration_tests", ["test"]);
-	grunt.config.merge({
-		coverage_threshold: {
-			statements: 0,
-			branches: 100,
-			functions: 0,
-			lines: 0
-		}
-	});
-};
+// module.exports = function (grunt) {
+// 	"use strict";
+// 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+// 	grunt.config.merge({compatVersion: "edge"});
+// 	grunt.registerTask("default", [
+// 		"clean",
+// 		"lint",
+// 		"build"
+// 	]);
+// 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
+// 	grunt.registerTask("unit_and_integration_tests", ["test"]);
+// 	grunt.config.merge({
+// 		coverage_threshold: {
+// 			statements: 0,
+// 			branches: 100,
+// 			functions: 0,
+// 			lines: 0
+// 		}
+// 	});
+// };
+
